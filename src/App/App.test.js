79486bc60355518390a678b2d63b5242e48a77cb
Nyssa Keller <nyssa1@gmail.com>
2018-01-31 19:06:45 -0700

    App test
    Test for initial states

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />)
+})
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('initial state of data, filmData, and favorites equal to empty array', () => {
+    expect(wrapper.state('data')).toEqual([]);
+    expect(wrapper.state('filmData')).toEqual([]);
+    expect(wrapper.state('favorites')).toEqual([]);
+  })
+
+  it('initial state of buttonClass and active equal to empty string', () => {
+    expect(wrapper.state('buttonClass')).toEqual('');
+    expect(wrapper.state('active')).toEqual('');
+  })
+
 });
