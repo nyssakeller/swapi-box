81b779d1ab840ff8d2d286b7a6cffce189a7314b
Nyssa Keller <nyssa1@gmail.com>
2018-01-31 18:49:06 -0700

    Finish card test

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -3,9 +3,17 @@ import { shallow } from 'enzyme';
 import Card from './Card.js';
 
 describe('Card', () => {
+  const mockName = 'Luke'
+  const mockDescription = 'Tatoonie'
+  const mockType = 'human'
+  const mockNumber = '9320304'
 
   it('match snapshot', () => {
-    const wrapper = shallow(<Card />)
+    const wrapper = shallow(<Card 
+                              name={mockName}
+                              description={mockDescription}
+                              type={mockType}
+                              number={mockNumber}/>)
     expect(wrapper).toMatchSnapshot();
   })
 
